236f1f4b0e1915a24046d69d6bf19b61bbe9b5f8
RaulS22 <rau1d3as@gmail.com>
2023-11-22 23:26:32 -0300

    organizando a branch de testes

--- fourier.py
+++ fourier.py
@@ -1,16 +1,54 @@
 from scipy.fft import fft
-import soundfile as sf
+import wave
 import numpy as np
 import matplotlib.pyplot as plt
 
-audio = 'FazOL.wav' #caminho do áudio TODO: arrumar isso
-audio_data, freq = sf.read(audio)
 
+class audio:
+    '''
+    Nessa classe o arquivo de áudio será tratado de
+    forma a ser entendido pelo resto do código. Serão
+    usados recursos de algumas bibliotecas.
+    '''
+
+    def __init__(self, audiopath):
+        audio_file = wave.open(audiopath, 'rb')
+        self.file_name = audiopath[audiopath.rfind('/') + 1:audiopath.rfind('.')]
+        # novo_nome =  + self.file_name #TODO:implementar a mudança de nome após plotar o gráfico e gerar o arquivo
+        
+        signal = audio_file.readframes(-1)
+        #signal = np.fromstring(signal, 'Int16')
+
+
+    def fourier(self):
+        pass
+        #TODO: implementar a transformação de Fourier
+
+
+
+    def plots(self):
+        pass
+        #TODO: implementar a classe de plotagem dos gráficos
+
+
+    '''
+    Aqui o arquivo de áudio está sendo criado da forma
+    "wave_write"
+    A variável signal recebe uma array com o conteúdo do
+    áudio
+    '''
+
+    
+
+
+
+
+
+
+
+################################
 '''
-O que está acontecendo na sexta linha é que
-audio_data está recebendo a informação do áudio em forma de array
-freq recebe a frequência (em Hz)
+Referências:
+https://docs.python.org/3/library/wave.html
+https://stackoverflow.com/questions/18625085/how-to-plot-a-wav-file
 '''
-
-y = fft(audio_data) #transformada de Fourrier do audio
-
