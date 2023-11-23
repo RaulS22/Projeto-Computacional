d94d420de7b98165ddbc98ee03a72f2c76bcb3be
pedrorpl <98560515+pedrorpl@users.noreply.github.com>
2023-11-23 15:13:34 -0300

    Update teste_wavelet.py
    Função para verificar possiveis problemas na reconstrução do sinal;
    Adição de comentários;
    Criação da função file_write() para escrever arquivos .wav apartir de objetos do tipo np.ndarray e frequência;

--- teste_wavelet.py
+++ teste_wavelet.py
@@ -8,27 +8,43 @@ from scipy.io import wavfile
 
 class audio_denoise:
     def __init__(self, path_to_file):
-        # Load audio file
-        self.y, self.sr = librosa.load(path_to_file)
-        # Get time values
+        # Load audio file saving the floating points y and audio rate
+        self.y, self.sr = librosa.load(path_to_file, sr = None)
+        # Get time values from number of floating points intensity divided by the audio rate
         self.time = np.arange(0, len(self.y))/ self.sr
-        # Get file name for naming the output
+        # Get file name to name the output file later on
         self.file_name = path_to_file[path_to_file.rfind('/') + 1:path_to_file.rfind('.')]
         # Start denoised signal variable
         self.denoised_signal = None
+        # Start the used transform name to name the output file later on
+        self.transform = None
 
-
-    def wavelet_transform(self, threshold):
+    
+    # Performs a 1 dimension wavelet transform with standard parameters if none are indicated
+    def wavelet_transform_1D(self, threshold, wavelet='db1', mode ='sym', level=None):
         # Perform a multi-level wavelet decomposition
-        coeffs = pywt.wavedec(self.y, 'db1', level=4)
-        # Set a threshold to nullify smaller coefficients (assumed to be noise)
+        coeffs = pywt.wavedec(self.y, wavelet, mode=mode, level=level)
+        # Set a threshold to nullify smaller coefficients assumed to be noise
         coeffs_thresholded = [pywt.threshold(c, threshold, mode='soft') for c in coeffs]
         # Reconstruct the signal from the thresholded coefficients
         self.denoised_signal = pywt.waverec(coeffs_thresholded, 'db1')
-        self.denoised_signal = np.array(self.denoised_signal)
-        output_file = "wavelet_" + self.file_name + ".wav"
+        # Used transform identifier
+        self.transform = 'wavelet_'
+
+    # Writes transformed signal into an audio file
+    def file_write(self):
+        # Name the file with it's extension
+        output_file = self.transform + self.file_name + ".wav"
+        # Writes file with the output_file name
         wavfile.write(output_file, int(self.sr), self.denoised_signal)
 
+    # Testing function to check if the usage of different natured libraries (librosa for loading and scipy.io.wavfile for writing) 
+    # in the audio reconstruction would insert any noise or loss of quality - (answer: No quality loss was noticed)
+    def test_audio_recon(self):
+        output_file = "reconstruction_TEST_" + self.file_name + ".wav"
+        wavfile.write(output_file, int(self.sr), self.y)
+
+    # Plots transform graphs for comparison
     def plot_denoising(self):
         fig = go.Figure()
 
@@ -44,9 +60,10 @@ class audio_denoise:
         fig.show()
 
 
+if __name__ == '__main__':
+    audio_file = "your_audio_file.wav"
 
-audio_file = "FazOL.wav"
+    denoising = audio_denoise(audio_file)
 
-denoising = audio_denoise(audio_file)
-
-denoising.wavelet_transform(0.01)
+    denoising.wavelet_transform_1D(0.005)
+    denoising.file_write()
